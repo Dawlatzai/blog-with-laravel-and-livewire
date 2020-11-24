96ad53433b3e336559a5cff11fec1273350f4f95
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-24 19:39:28 +0430

    Add tailwindcss style typography lib

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'Laravel Blog')</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/@tailwindcss/typography@0.2.x/dist/typography.min.css" />
   @livewireStyles
 </head>
 <body>
 
   <!-- header component -->
   <x-header></x-header>
-  
+
   @yield('content')
 
   @livewireScripts
