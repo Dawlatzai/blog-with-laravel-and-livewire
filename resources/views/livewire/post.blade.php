1cccb8381781f8682fc23bce3568d1b55770d767
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-24 19:40:21 +0430

    Add tailwind classes for post

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="max-w-4xl mx-auto py-20">
+    <div class="max-w-4xl mx-auto py-20 prose lg:prose-xl">
         <h1>{{ $post->title }}</h1>
         <p>{!! $post->body !!}</p>
     </div>
